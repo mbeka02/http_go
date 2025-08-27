caccca77aaa797846c6119ea923dcab14f36ef6a
mbeka02 <antonymbeka@gmail.com>
2025-08-27 23:29:55 +0300

    new header parsing tests

--- internal/headers/headers_test.go
+++ internal/headers/headers_test.go
@@ -15,8 +15,17 @@ func TestHeaders(t *testing.T) {
 	require.NoError(t, err)
 	require.NotNil(t, headers)
 	assert.Equal(t, "localhost:42069", headers["Host"])
-	assert.Equal(t, 23, n)
-	assert.False(t, done)
+	assert.Equal(t, 25, n)
+	assert.True(t, done)
+
+	// Test: Valid 2 headers with existing headers
+	data = []byte("Key1: Value1\r\n  Key2: Value2 \r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "Value1", headers.Get("Key1"))
+	assert.Equal(t, "Value2", headers.Get("Key2"))
+	assert.True(t, done)
 
 	// Test: Invalid spacing header
 	headers = NewHeaders()
@@ -25,4 +34,22 @@ func TestHeaders(t *testing.T) {
 	require.Error(t, err)
 	assert.Equal(t, 0, n)
 	assert.False(t, done)
+
+	// Test: Valid single header with extra whitespace
+	headers = NewHeaders()
+	data = []byte("Host:             localhost:42069                \r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	require.NotNil(t, headers)
+	assert.Equal(t, "localhost:42069", headers["Host"])
+	assert.Equal(t, 53, n)
+	assert.True(t, done)
+
+	// Test:  Valid Done
+	headers = NewHeaders()
+	data = []byte("\r\n\r\n")
+	n, done, err = headers.Parse(data)
+	require.NoError(t, err)
+	assert.Equal(t, 2, n)
+	assert.True(t, done)
 }
